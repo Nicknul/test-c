b3ea7485b1487a075fe445a7a4bb2182b48d957a
정호연 <jea7730@gmail.com>
2024-08-02 11:37:18 +0900

    :sparkles: 정호연 : 셀렉트한 테이블에서 값 가져오기 요청

--- database/routers.py
+++ database/routers.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException
-from typing import List
-from models import SearchRequest, UserDTO
-from database import search_users_by_name, get_table_names
+from typing import List, Dict
+from models import SearchRequest, UserDTO, TableRequest
+from database import search_users_by_name, get_table_names, search_data_by_table_and_name
 
 user_router = APIRouter()
 
@@ -23,5 +23,17 @@ async def get_tables():
     try:
         tables = get_table_names('테스트.db')
         return tables
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+data_router = APIRouter()
+
+@data_router.post("/data", response_model=List[Dict])
+async def get_data(request: TableRequest):
+    try:
+        rows = search_data_by_table_and_name(request.table, request.name)
+        if not rows:
+            raise HTTPException(status_code=404, detail="Data not found")
+        return rows
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
