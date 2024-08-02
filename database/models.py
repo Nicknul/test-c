a212fe396eece9d448db58f0f5d6d5836a6cb481
정호연 <jea7730@gmail.com>
2024-08-02 11:37:51 +0900

    :egg: 정호연 : DTO 추가

--- database/models.py
+++ database/models.py
@@ -7,3 +7,7 @@ class UserDTO(BaseModel):
     이름: str  # 이름 필드는 문자열 타입이다.
     아이디: str  # 아이디 필드는 문자열 타입이다.
     비밀번호: int  # 비밀번호 필드는 정수 타입이다.
+
+class TableRequest(BaseModel):
+    table: str  # table 필드는 문자열 타입이다.
+    name: str  # name 필드는 문자열 타입이다.
