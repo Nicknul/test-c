8996030d6d32b968efafd72f539afec2d94cc8f5
정호연 <jea7730@gmail.com>
2024-08-02 10:45:10 +0900

    :sparkles: 정호연 : 찾은 값을 반환할 때 가공하는 모듈

---
+++ database/models.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+class SearchRequest(BaseModel):
+    name: str  # name 필드는 문자열 타입이다.
+
+class UserDTO(BaseModel):
+    이름: str  # 이름 필드는 문자열 타입이다.
+    아이디: str  # 아이디 필드는 문자열 타입이다.
+    비밀번호: int  # 비밀번호 필드는 정수 타입이다.
